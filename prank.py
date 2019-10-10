85e4bf7b0c838b4817e66eda3a25e10560d6f2e6
HystERICKal <nyoroerick12@gmail.com>
2019-10-10 16:33:56 +0200

    initial commit

---
+++ prank.py
@@ -0,0 +1,15 @@
+import webbrowser 
+import time 
+import random
+
+
+while True:
+    sites = random.choice(['www.google.co.za/search?q=sexy+nude+gays&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiitN-c9YDlAhXHa8AKHbARCIAQ_AUIESgB&biw=1366&bih=691',
+                            'www.google.co.za/search?biw=1366&bih=691&tbm=isch&sa=1&ei=O1iWXcWBDc2fjLsPo_m2aA&q=sexy+nude+shemales&oq=sexy+nude+shemales&gs_l=img.3...29026.33626..33750...0.0..0.227.3555.0j4j13......0....1..gws-wiz-img.......0.gA2Idnzcsao&ved=0ahUKEwiFi7ae9YDlAhXND2MBHaO8DQ0Q4dUDCAY&uact=5',
+                            'www.google.co.za/search?biw=1366&bih=691&tbm=isch&sa=1&ei=XliWXaedN5CiUruLseAP&q=fat+people+porn&oq=fat+people+porn&gs_l=img.3...27197.30975..31856...0.0..0.364.3767.2-12j3......0....1..gws-wiz-img.......0j0i67.hmZqdwXj23w&ved=0ahUKEwinxLiv9YDlAhUQkRQKHbtFDPwQ4dUDCAY&uact=5',
+                            'www.google.co.za/search?biw=1366&bih=691&tbm=isch&sa=1&ei=gFiWXcmJOZCkUrW1mqgI&q=i+love+sexy+dicks&oq=i+love+sexy+dicks&gs_l=img.3...26458.32317..32975...1.0..0.376.4441.0j1j14j3......0....1..gws-wiz-img.......0j0i67j0i8i30j0i30j0i24.NgQea42SIOg&ved=0ahUKEwjJydW_9YDlAhUQkhQKHbWaBoUQ4dUDCAY&uact=5',
+                            'www.google.co.za/search?biw=1366&bih=691&tbm=isch&sa=1&ei=pViWXafLCYyCjLsPlNO3wAQ&q=sexy+lesbian+porn&oq=sexy+lesbian+porn&gs_l=img.3...26815.32251..32739...0.0..0.360.4131.0j2j12j3......0....1..gws-wiz-img.......0j0i67.Zd8qym5b8os&ved=0ahUKEwjnsfjQ9YDlAhUMAWMBHZTpDUgQ4dUDCAY&uact=5']) 
+    visit = "http://{}".format(sites)
+    webbrowser.open(visit)
+    seconds = random.randrange(5,10)
+    time.sleep(seconds)
